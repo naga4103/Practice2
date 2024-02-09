684e73d0034c5c9522c305a7616d7152e6e5693a
naga4103 <naganaveena103@gmail.com>
2024-02-09 12:58:38 +0530

    Practice2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+  console.log("hiiiiii");
+  let ref = useRef(1);
+  let ref2 = useRef(null);
+  console.log("Ref...", ref);
+  console.log("Ref2..", ref2);
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    ref.current = ref.current + 1;
+    // alert("You clicked " + ref.current + " times!");
+    console.log("Ref Value...", ref.current); //ref.current value is updating on every click.
+  }
+
+  // useEffect(() => {
+  //   ref.current = 4;
+  //   console.log("Refff", ref);
+  //   console.log("Ref value...", ref.current);
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center">
+      {/* <p>{`Count: ${count}`}</p> */}
+      <p>{ref.current}</p>
+      {/* displays initial value of ref--as it is 1,it display 1..does not update on updation of ref.current */}
+      {/* <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click Me!
+      </button> */}
+
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
